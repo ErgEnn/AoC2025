75c53e4a7ab1ce20d4d18fd8b3e62470e78c694a
Ergo Enn <ergo97@gmail.com>
2025-12-01 07:25:13 +0200

    Day 01

--- aoc2025_01/aoc2025_01.go
+++ aoc2025_01/aoc2025_01.go
@@ -2,32 +2,83 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 const example = `
-1 2
-3 4
-10 20
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
 `
 
 const real = `
-1 2
-3 4
-10 20
 `
 
 func main() {
-	part1(example)
+	part1(real)
+	part2(real)
 }
 
-func prep(input string) {
-
+func prep(input string) []string {
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	return lines
 }
 
 func part1(input string) {
-	fmt.Println(input)
+	lines := prep(input)
+	current := 50
+	answer := 0
+	for _, l := range lines {
+		offset, _ := strconv.Atoi(l[1:])
+		switch l[0] {
+		case 'L':
+			current = (current - offset + 100) % 100
+		case 'R':
+			current = (current + offset + 100) % 100
+		}
+		if current == 0 {
+			answer++
+		}
+	}
+	fmt.Println(answer)
 }
 
 func part2(input string) {
-	fmt.Println(input)
+	lines := prep(input)
+	current := 50
+	answer := 0
+	for _, l := range lines {
+		offset, _ := strconv.Atoi(l[1:])
+		switch l[0] {
+		case 'L':
+			current = current - offset
+		case 'R':
+			current = current + offset
+		}
+		for {
+			if current == 0 {
+				break
+			}
+			if current < 0 {
+				answer++
+				current += 100
+				continue
+			}
+			if current >= 100 {
+				answer++
+				current -= 100
+				continue
+			}
+			break
+		}
+	}
+	fmt.Println(answer)
 }
